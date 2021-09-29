55a7d9e6429cbd316ff30b4b9838a879d95d7327
Waruta <62998111+WarutaShinken@users.noreply.github.com>
2021-09-29 10:51:00 +1000

    Finalised For Version 3

--- silo.py
+++ silo.py
@@ -11,6 +11,8 @@ from chia.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
 from sqlite3 import Error
 from pathlib import Path
 
+import os
+
 SILO_ROOT_PATH=Path(__file__).parent
 FORKS_LIST_FILE=(SILO_ROOT_PATH / "forks.yaml").resolve()
 
@@ -36,36 +38,60 @@ def main(argv):
     argumentList = sys.argv[1:]
     
     # Short options
-    options = "hlb:s:"
+    options = "ha:b:s:l"
     
     # Long options
-    long_options = ["help", "balance", "spent", "list-forks"]
+    long_options = ["help", "reward-address=", "balance=", "spent=", "list-forks"]
     
     try:
         # Parsing argument
         arguments, values = getopt.getopt(argumentList, options, long_options)
+        
+        #No Arguments
+        if len(arguments) == 0:
+            if os.name == 'nt': #Windows
+                print('Usage: silo.py -h')
+            elif os.name == 'posix': #Unix/POSIX-Based
+                print('Usage: python3 silo.py -h')
+            else:
+                print("I'm not even sure what platform you're running, but whatever, please provide an argument.")
+            
+            sys.exit(0)
+        
         # checking each argument
         for currentArgument, currentValue in arguments:
             if currentArgument in ("-h", "--help"):
                 print("NAME")
                 print ("\tsilo (WarutaShinken fork) -- display your crypto (Chia/altcoin) wallet balance as a raw number.")
-                print ("\tIntended for use in shell scripts.")
+                print ("\tExtended for use in shell scripts.")
+                
+                print('')
+                
                 print("DESCRIPTION")
                 print ("\tsilo -h | --help - Display this help.")
+                print ("\tsilo -a <address> | --reward-address <receive-address> - Display a summary of your wallet balance and spendings")
                 print ("\tsilo -b <receive-address> | --balance <receive-address> - Display your wallet address balance")
                 print ("\tsilo -s <receive-address> | --spent <receive-address> - Display how many coins from your wallet were spent*")
                 print ("\tsilo -l | --list-forks - Display currently supported forks from the {} file".format(FORKS_LIST_FILE))
+                
+                print('')
+                
                 print("EXAMPLE")
-                print ("\tpython silo.py -b xch18krkt5a9jlkpmxtx8akfs9kezkuldpsn4j2qpxyycjka4m7vu6hstf6hku\n")
-                print("ANNOTATION")
+                print ("\tpython silo.py -b xch18krkt5a9jlkpmxtx8akfs9kezkuldpsn4j2qpxyycjka4m7vu6hstf6hku")
+                
+                print('')
+                
+                print("ADDITIONAL NOTES")
                 print ("\t*I'm not too sure what it means for a coin to be 'spent'. I will clarify this in an update when someone informs")
                 print ("\tme. Feel free to make an issue or PR to clarify it.")
+            elif currentArgument in ("-a", "--reward-address"):
+                print_summary(currentValue)
+            elif currentArgument in ("-b", "--balance"):
+                print_coins(currentValue, False)
+            elif currentArgument in ("-s", "--spent"):
+                print_coins(currentValue, True)
             elif currentArgument in ("-l", "--list-forks"):
                 load_fork_names(print_list=True)
-            elif currentArgument in ("-b", "--balance"):
-                get_balance(sys.argv[2], False)
-            elif currentArgument in ("-s", "--spent"):
-                get_balance(sys.argv[2], True)
             
     except getopt.error as err:
         # output error, and return with an error code
@@ -108,19 +134,24 @@ def db_for_token(token_name):
         print("ERROR: blockchain path does not exist: ", full_path_to_db)
         sys.exit(1)
 
-def get_db_file_from_address(address):
-    load_fork_names();
-    
+def get_token(address):
     for key in token_to_data_dir_mapping:
         if key in address[0:len(key)]:
-            # Reset units of measurement if non-standard (i.e. ChiaRose)
-            global UNITS_OF_MEASUREMENT
-            UNITS_OF_MEASUREMENT = units_of_measurement(key)
-            return db_for_token(key)
+            return key
     
     print("ERROR: Undefined blockchain, add your own to the {} list first and run the script again.".format(FORKS_LIST_FILE))
     sys.exit(1)
+
+def get_db_file_from_address(address):
+    load_fork_names();
     
+    key = get_token(address)
+    
+    # Reset units of measurement if non-standard (i.e. ChiaRose)
+    global UNITS_OF_MEASUREMENT
+    UNITS_OF_MEASUREMENT = units_of_measurement(key)
+    
+    return db_for_token(key)
 
 def units_of_measurement(fork_token_name):
     
@@ -135,18 +166,24 @@ def units_of_measurement(fork_token_name):
         
     return UNITS_OF_MEASUREMENT
 
-def get_balance(address, count_spent_coins):
+def get_puzzle_hash(address):
+    return decode_puzzle_hash(address).hex()
+
+#Returns all the coin records associated with the wallet address specified.
+def get_rows(address, verbose=False):
     # print("Retreiving the wallet balance for:", address)
     
     # convert farmer address to puzzle hash
-    puzzle_hash_bytes = decode_puzzle_hash(address)
-    puzzle_hash = puzzle_hash_bytes.hex()
-    #print(f"Searching for puzzle_hash: 0x{puzzle_hash}")
+    puzzle_hash = get_puzzle_hash(address)
     
     # sql for puzzle hash
     try:
         db_file_to_load = get_db_file_from_address(address)
-        #print(db_file_to_load)
+        
+        if verbose:
+            print(f"Found blockchain database: {db_file_to_load}")
+            print(f"Searching for puzzle_hash: 0x{puzzle_hash}")
+        
         conn = create_connection(db_file_to_load)
         
         dbcursor = conn.cursor()
@@ -162,26 +199,52 @@ def get_balance(address, count_spent_coins):
         
         dbcursor.execute("SELECT * FROM coin_record WHERE puzzle_hash=?", (puzzle_hash,))
         
-        rows = dbcursor.fetchall()
-        
-        coin_count = 0
-        
-        for row in rows:
-            
-            #print(int.from_bytes(row[7], 'big'))
-            xch_raw=int.from_bytes(row[7], 'big')
-            #print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], xch_raw, row[8])
-            xch=xch_raw/UNITS_OF_MEASUREMENT
-            # print("{:.12f}".format(xch))
-            is_coin_spent = row[3]
-            
-            #If the coin matches the criteria we're after...
-            if is_coin_spent == count_spent_coins:
-                coin_count += xch
-            
-        print(format(coin_count, '.12f'), end="")
+        return dbcursor.fetchall()
     except Error as e:
         print(e)
+
+def transaction_size(row):
+    return int.from_bytes(row[7], 'big') / UNITS_OF_MEASUREMENT
+
+def coin_is_spent(row):
+    return row[3]
+
+def print_coins(address, count_spent_coins):
+    rows = get_rows(address)
+    coin_count = 0
+    
+    for row in rows:
+        xch = transaction_size(row)
+        
+        #If the coin matches the criteria we're after...
+        if coin_is_spent(row) == count_spent_coins:
+            coin_count += xch
+    
+    print(format(coin_count, '.12f'), end="")
+
+def print_summary(address):
+    rows = get_rows(address, True)
+    token = get_token(address).upper()
+    
+    wallet_balance = 0
+    wallet_spendings = 0
+    
+    for row in rows:
+        xch = transaction_size(row)
+        
+        if coin_is_spent(row):
+            wallet_spendings += xch
+        else:
+            wallet_balance += xch
+    
+    print('')
+    print('Summary:')
+    print('')
+    print(f"Wallet Address    : {address}")
+    print(f"Wallet Puzzle Hash: 0x{get_puzzle_hash(address)}")
+    print('')
+    print(f"Wallet Balance    : {wallet_balance:.12f} {token}")
+    print(f"Wallet Spendings  : {wallet_spendings:.12f} {token}")
 
 def create_connection(db_file):
     conn = None
